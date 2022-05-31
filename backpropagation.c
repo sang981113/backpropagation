48234cd0540d860e1caba7ae14ed4e025be7f262
sang981113 <75288953+sang981113@users.noreply.github.com>
2022-05-31 20:47:47 +0900

    update logic

--- backpropagation.c
+++ backpropagation.c
@@ -1,6 +1,10 @@
 #define MAX_LEN 10000000
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+double sigmoid(double x);
+double sigmoid_prime(double x);
 
 int main()
 {
@@ -10,21 +14,34 @@ int main()
 	int n = 100;
 	int p = 20;
 	int m = 10;
+	double lr = 0.5;
 	int i, j, k, l;
 	int epoch;
 	int loop = 0;
 
-	float input_unit[100] = { 0, };
-	float hidden_unit[20] = { 0, };
-	float output_unit[10] = { 0, };
+	double dot_product = 0;
+	double input_unit[101] = { 0, };
+	double hidden_in[21] = { 0, };
+	double hidden_unit[21] = { 0, };
+	double hidden_error[21] = { 0, };
+	double output_in[10] = { 0 };
+	double output_unit[10] = { 0, };
+	double output_error[10] = { 0, };
+	double output_error_in[10] = { {0} };
+	double target[10] = { 0, };
 
-	float v[100][20] = {{0}}; //weight initialization
-	float w[20][10] = {{0}};
+	//weight initialization
+	double v[101][20] = { {0} }; 
+	double dv[101][20] = { {0} };
+	double nv[101][20] = { {0} };
+	double w[21][10] = { {0} };
+	double dw[21][10] = { {0} };
+	double nw[21][10] = { {0} };
 
-	float** x_train = (float**)malloc(sizeof(float*) * pattern_num); //malloc array x_train[4000][100]
+	double** x_train = (double**)malloc(sizeof(double*) * pattern_num); //malloc array x_train[4000][100]
 	for (i = 0; i < pattern_num; i++)
 	{
-		x_train[i] = (float*)malloc(sizeof(float) * feature_num);
+		x_train[i] = (double*)malloc(sizeof(double) * feature_num);
 	}
 
 	int* y_train = (int*)malloc(sizeof(int) * pattern_num); //malloc array y_train[4000]
@@ -56,44 +73,81 @@ int main()
 	{
 		for (l = 0; l < pattern_num; l++)
 		{
+			//forward propagation
 			for (i = 1; i <= n; i++) // insert x_train values into input units
 			{
 				input_unit[i] = x_train[l][i];
 			}
 			for (j = 1; j <= p; j++)
 			{
-
+				dot_product = 0;
+				for (i = 1; i <= n; i++)
+				{
+					dot_product += input_unit[i] * v[i][j];
+				}
+				hidden_in[j] = v[0][j] + dot_product;
+				hidden_unit[j] = sigmoid(hidden_in[j]);
 			}
 			for (k = 1; k <= m; k++)
 			{
-
+				dot_product = 0;
+				for (j = 1; j <= p; j++)
+				{
+					dot_product += hidden_unit[j] * w[j][k];
+				}
+				output_in[k] = w[0][k] + dot_product;
+				output_unit[k] = sigmoid(output_in[k]);
 			}
+
+			//back propagation
 			for (i = 1; i <= m; i++)
 			{
-
+				output_error[i] = (target[i] - output_unit[i]) * sigmoid_prime(output_in[i]);
+				for (j = 0; j <= p; j++)
+				{
+					dw[j][i] = lr * output_error[j] * hidden_unit[j];
+				}
 			}
 			for (i = 1; i <= p; i++)
 			{
+				dot_product = 0;
+				for (k = 1; k <= m; k++)
+				{
+					dot_product += output_error[k] * w[i][k];
+				}
+				output_error_in[i] = dot_product;
+				hidden_error[i] = output_error_in[i] * sigmoid_prime(output_in[i]);
 				for (j = 0; j <= n; j++)
 				{
-
+					dv[j][i] = lr * hidden_error[j] * input_unit[i];
 				}
 			}
+			//update wieghts and biases
 			for (i = 1; i <= m; i++)
 			{
 				for (j = 0; j <= p; j++)
 				{
-
+					nw[j][i] = w[j][i] + dw[j][i];
 				}
 			}
 			for (j = 1; j <= p; j++)
 			{
 				for (i = 0; i <= n; i++)
 				{
-
+					nv[j][i] = v[j][i] + dv[j][i];
 				}
 			}
 		}
 		loop++;
 	}
+}
+
+double sigmoid(double x)
+{
+	return 1 / (1 + exp(-x));
+}
+
+double sigmoid_prime(double x)
+{
+	return sigmoid(x) * (1 - sigmoid(x));
 }
